fdc125aad664f707be2692487551aa29614d44d2
Margot Konig <mkonig411@gmail.com>
2019-01-31 23:44:00 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let tracker = 0
+
+const checkKeyCode = () => {
+  const whatTheyPressed = event.checkKeyCode
+  if (whatTheyPressed === code[tracker]) {
+  tracker += 1
+} else {
+  tracker = 0
+}
+console.log(tracker)
+}
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+// keydown
 }
+
+init()
